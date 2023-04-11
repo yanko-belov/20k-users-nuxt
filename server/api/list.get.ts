dd61613f9edf82ce1391770d21632acbc9a64d61
Yanko Belov <belov.yanko@gmail.com>
2023-04-11 19:46:00 +0300

    feat: Add BE API that gets the data from MongoDB

---
+++ server/api/list.get.ts
@@ -0,0 +1,27 @@
+import { MongoClient } from "mongodb";
+
+export default defineEventHandler(async (event) => {
+  const uri = useRuntimeConfig().mongodbUri;
+  try {
+    const queryParams = getQuery(event);
+    const offset = parseInt((queryParams?.offset as string) || "0", 10);
+    const filter = (queryParams?.filter as string) || "";
+    const find = { country: { $regex: filter, $options: "i" } };
+
+    const client = new MongoClient(import.meta.env.NUXT_MONGODB_URI as string);
+    const database = client.db(import.meta.env.NUXT_MONGODB_DB as string);
+
+    const usersCollection = database.collection("users");
+    const totalCount = await usersCollection.countDocuments(find);
+
+    const data = await usersCollection
+      .find(find)
+      .skip(offset)
+      .limit(500)
+      .toArray();
+    await client.close();
+    return { users: data, totalCount, uri: uri };
+  } catch (error) {
+    return { users: [], totalCount: 0, error, uri: uri };
+  }
+});
