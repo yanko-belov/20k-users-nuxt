05b1f78d5e77f52a8f3423d4d85a1c734d821673
Yanko Belov <belov.yanko@gmail.com>
2023-04-12 16:11:39 +0300

    feat: Add Tailwind CSS and Sass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ["~/assets/css/main.scss"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   modules: ["@sidebase/nuxt-auth"],
 });
