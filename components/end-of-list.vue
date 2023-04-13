0c054e9080676b5d64f5ba7c1a73d82152c24cb2
Yanko Belov <belov.yanko@gmail.com>
2023-04-13 09:28:07 +0300

    feat: Create EndOfList component

---
+++ components/end-of-list.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="mx-auto my-5 max-w-screen-md text-center">
+    <div class="flex justify-center">
+      <img src="/img/the-end.webp" alt="The End Hasbi" />
+    </div>
+    <h3 class="mt-3 font-bold">Congrats! This is the end of the list!</h3>
+  </div>
+</template>
